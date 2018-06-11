dd5f592c4a9965e1e91c7eb7c58f6718689d32ec
Zhao Tian <Zhao Tian@dev.igiltd.local>
2018-06-11 20:57:35 +0100

    first to do draft

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async System.Threading.Tasks.Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
+        }
+    }
+}
